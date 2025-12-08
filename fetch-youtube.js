8175e5f0a8eb20f385bec757e518332617b1df63
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-12-08 15:51:39 +0000

    feat: set up initial fetchVideos to fetch YouTube channel video data

--- fetch-youtube.js
+++ fetch-youtube.js
@@ -1,4 +1,27 @@
 import "dotenv/config";
 
 const apiKey = process.env.YOUTUBE_API_KEY;
-console.log(apiKey);
+const channelId = process.env.YOUTUBE_CHANNEL_ID;
+const url = `
+https://www.googleapis.com/youtube/v3/search?
+key=${apiKey}
+&channelId=${channelId}
+&part=snippet,id
+&maxResults=20
+&order=date
+&type=video
+&videoEmbeddable=true
+`;
+
+const fetchVideos = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+fetchVideos();
+console.log({ apiKey, channelId });
